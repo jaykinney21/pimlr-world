5583f45643b801b1a0921b7d49346eea208d9b43
Jay Kinney <jkinney@flip-tech.com>
2026-06-18 11:08:48 -0700

    PIMLR World import: batch 5

---
+++ Assets/AssetsByME/Scripts/DreamCar01Track.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DreamCar01Track : MonoBehaviour
+{
+
+    public GameObject TheMarker;
+    public List<GameObject> Markers = new List<GameObject>();
+
+    public int MarkTracker = 0;
+
+    void Update()
+    {
+        if (MarkTracker < Markers.Count)
+        {
+            TheMarker.transform.position = Markers[MarkTracker].transform.position;
+        }
+    }
+
+    private IEnumerator OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "DreamCar01")
+        {
+            this.GetComponent<BoxCollider>().enabled = false;
+            MarkTracker = (MarkTracker + 1) % Markers.Count;  // This will loop back to 0 if MarkTracker equals the number of Markers.
+            yield return new WaitForSeconds(1);
+            this.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
